81a69aad7d8f3b245d6d4def4fa56638f9f9bdb2
aekkaphonz <89302065+aekkaphonz@users.noreply.github.com>
2024-01-16 15:10:31 +0700

    Add files via upload
    Morder_flutter

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,134 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_new/widgets/avatar_widget.dart';
+import 'package:flutter_new/widgets/location_widget.dart';
+import 'package:flutter_new/widgets/name_widget.dart';
+import 'package:flutter_new/widgets/picture_widget.dart';
+import 'package:flutter_new/widgets/social_widget.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          leading: const Icon(
+            Icons.menu,
+            color: Colors.white,
+            size: 24,
+
+          ),
+          title: const Text('My Profile',style:TextStyle(color: Colors.white) ,),
+          actions: const [
+            Padding(
+              padding:EdgeInsets.fromLTRB(0, 0, 5, 0),
+              child: Icon(
+                Icons.search,
+                color: Colors.white,
+                size: 24,
+              ),
+
+              ),
+              Padding(
+              padding:EdgeInsets.fromLTRB(0, 0, 10, 0),
+              child: Icon(
+                Icons.person_add,
+                color: Colors.white,
+                size: 24,
+              ),
+
+              ),
+              
+
+          ],
+
+
+        ),
+        body: const Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        AvatarWidget(),
+        SizedBox(
+          height: 20,
+        ),
+        NameWidget(),
+        SizedBox(
+          height: 20,
+        ),
+        LocationWidget(),
+        Text(
+          "photography is the story i fail to put into words",style: TextStyle(
+            fontFamily: 'poppins',
+            fontSize: 15,
+            color: Colors.white,
+            fontWeight: FontWeight.w200,
+            decoration: TextDecoration.none
+          ),
+        ),
+        SizedBox(height: 20,),
+        SocialWidget(),
+        SizedBox(height: 40,),
+        
+        Row(children: [
+          SizedBox(width: 10,),
+          Text('Photos',style: TextStyle(
+          fontFamily:"poppins",
+          fontSize: 20,
+          color: Colors.black,
+          fontWeight: FontWeight.w700,
+          decoration: TextDecoration.none ,
+          
+          ),
+          
+          
+          ),
+        ],
+          
+          
+        ),
+
+        PictureWidget()
+      ],
+
+        ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+          ),
+          BottomNavigationBarItem(
+          icon: Icon(Icons.business),
+          label: 'Business',
+          ),
+          BottomNavigationBarItem(
+          icon: Icon(Icons.school),
+          label: 'School',
+          ),
+          ],
+          selectedItemColor:Colors.black,
+      ),
+      ),
+      
+      
+      
+    );
+    
+    
+  }
+}
+
+
+
+
+
+
+
